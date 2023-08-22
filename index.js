1be09fcc20ac65e74707135684142bb0c6a6fe3a
Niteesh <202198@ibsplc.com>
2023-08-22 15:34:22 +0530

    febnocii series

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 "Welcome to javascript projects";
+
+function fabnociseries(n) {
+  if ((n = 1)) return [0, 1];
+
+  var s = fabnociseries(n - 1);
+
+  s.push(s[s.length - 1] + s[s.length - 2]);
+
+  return s;
+}
+
+console.log(fabnociseries(8));
